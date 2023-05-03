d7b62f717e8765335ff17f92440bbda16a5aae56
mikhail <mishaprizby@gmail.com>
2023-05-04 01:07:59 +0300

    создание заказов

--- farmer/order/models.py
+++ farmer/order/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from store.models import Product
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(
+    User, on_delete=models.CASCADE, blank=True, null=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created']
+        indexes = [
+            models.Index(fields=['user']),
+            models.Index(fields=['-created']),
+        ]
+
+    def __str__(self) -> str:
+        return f'Order {self.id}'
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='order_items')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self) -> str:
+        return f'{self.id}'
+
+    def get_cost(self):
+        return self.price * self.quantity
--- farmer/order/views.py
+++ farmer/order/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import OrderItem
+from .forms import OrderCreationForm
+from cart.cart import Cart
 
-# Create your views here.
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreationForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            if request.user.is_authenticated:
+                order.user = request.user
+                order.save()
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item['product'], quantity=item['quantity'], price = item['price'])
+            cart.clear()
+            return render(request, 'orders/order/created.html', {'order': order})
+    else:
+        form = OrderCreationForm()
+    return render(request, 'orders/order/create.html', {'form': form, 'cart': cart})
