966a65b62bb61e27dcd2b09ea4bc5778835a565f
mikhail <mishaprizby@gmail.com>
2023-05-04 00:53:21 +0300

    добавил создание групп и добавление юзеров в группы

--- farmer/users/models.py
+++ farmer/users/models.py
@@ -5,14 +5,18 @@ from django.contrib.auth.models import User
 class Seller(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     photo = models.ImageField(upload_to='sellers/', blank=True)
-    location = models.CharField(max_length=100, blank=True)
+    address = models.CharField(max_length=100, blank=True)
     phone_number = models.CharField(max_length=20, blank=True)
+    is_seller = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.user.username
+
 
 class Buyer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=20)
+    is_buyer = models.BooleanField(default=True)
 
-
-
-
-
+    def __str__(self):
+        return self.user.username
